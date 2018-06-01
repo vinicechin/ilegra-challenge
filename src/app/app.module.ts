0e4f6cfb644ed654bf3c623654fcd2f8ece8929e
Vinicius Cechin <vinicechin@gmail.com>
2018-06-01 13:27:33 -0300

    Added bootstrap to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { SwapiService } from './swapi.service';
 import { FilmsComponent } from './films/films.component';
 import { CharacterComponent } from './character/character.component';
+import { FilmComponent } from './films/film/film.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FilmsComponent,
-    CharacterComponent
+    CharacterComponent,
+    FilmComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule.forRoot()
   ],
   providers: [SwapiService],
   bootstrap: [AppComponent]
