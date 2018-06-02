d7567762de57feeb52c96b6aed53fc604d4fd5aa
Vinicius Cechin <vinicechin@gmail.com>
2018-06-01 22:41:46 -0300

    Added other film elements to view

--- src/app/films/film-details/film-details.component.ts
+++ src/app/films/film-details/film-details.component.ts
@@ -10,6 +10,10 @@ import { Film } from '../../film.model';
 export class FilmDetailsComponent implements OnInit {
   @Input() selFilm: Film;
   characterNames: string[] = [];
+  starshipNames: string[] = [];
+  vehicleNames: string[] = [];
+  specieNames: string[] = [];
+  planetNames: string[] = [];
 
   constructor(private swapiService: SwapiService) {}
 
@@ -19,6 +23,10 @@ export class FilmDetailsComponent implements OnInit {
   ngOnChanges() {
     if (this.selFilm) {
       this.getCharactersName()
+      this.getStarshipsName()
+      this.getVehiclesName()
+      this.getSpeciesName()
+      this.getPlanetsName()
     }
   }
 
@@ -29,6 +37,54 @@ export class FilmDetailsComponent implements OnInit {
         .subscribe(
           (character: any) => {
             this.characterNames.push(character.name)
+          }
+        )
+    }
+  }
+
+  getStarshipsName() {
+    this.starshipNames = []
+    for (let starshipUrl of this.selFilm.starships) {
+      this.swapiService.getUnitaryData(starshipUrl)
+        .subscribe(
+          (starship: any) => {
+            this.starshipNames.push(starship.name)
+          }
+        )
+    }
+  }
+
+  getVehiclesName() {
+    this.vehicleNames = []
+    for (let vehiclesUrl of this.selFilm.vehicles) {
+      this.swapiService.getUnitaryData(vehiclesUrl)
+        .subscribe(
+          (vehicle: any) => {
+            this.vehicleNames.push(vehicle.name)
+          }
+        )
+    }
+  }
+
+  getSpeciesName() {
+    this.specieNames = []
+    for (let specieUrl of this.selFilm.species) {
+      this.swapiService.getUnitaryData(specieUrl)
+        .subscribe(
+          (specie: any) => {
+            this.specieNames.push(specie.name)
+          }
+        )
+    }
+  }
+
+  getPlanetsName() {
+    this.planetNames = []
+    for (let planetUrl of this.selFilm.planets) {
+      this.swapiService.getUnitaryData(planetUrl)
+        .subscribe(
+          (planet: any) => {
+            this.planetNames.push(planet.name)
           }
         )
     }
