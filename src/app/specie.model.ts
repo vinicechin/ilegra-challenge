cef59be034464be0e23fd1b5508ac3c1208e983d
Vinicius Cechin <vinicechin@gmail.com>
2018-06-02 11:55:00 -0300

    Added species model

---
+++ src/app/specie.model.ts
@@ -0,0 +1,44 @@
+export class Specie { 
+  constructor(public name: string,
+              public classification: string,
+              public designation: string,
+              public average_height: string,
+              public skin_colors: string,
+              public hair_colors: string,
+              public eye_colors: string,
+              public average_lifespan: string,
+              public homeworld: string,
+              public language: string,
+              public characters: string[],
+              public films: string[],
+              public created: string,
+              public edited: string
+              ) {} 
+
+  // Build film structure based on film model
+  public static buildSpeciesStructure(species) {
+    const speciesArray: Specie[] = [];
+    for (let specie of species) {
+      const newSpecie = new Specie(
+        specie.name,
+        specie.classification,
+        specie.designation,
+        specie.average_height,
+        specie.skin_colors,
+        specie.hair_colors,
+        specie.eye_colors,
+        specie.average_lifespan,
+        specie.homeworld,
+        specie.language,
+        specie.characters,
+        specie.films,
+        specie.created,
+        specie.edited,
+      )
+
+      speciesArray.push(newSpecie)
+    }
+    return speciesArray;
+  }
+}
+ 
