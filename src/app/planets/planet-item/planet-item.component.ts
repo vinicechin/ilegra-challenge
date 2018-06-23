9a9b09faf53dd8cc220b70327aad0321a1c5fae2
Vinicius Cechin <vinicechin@gmail.com>
2018-06-23 16:09:51 -0300

    Added planet item styles

--- src/app/planets/planet-item/planet-item.component.ts
+++ src/app/planets/planet-item/planet-item.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router'; 
+
+import { SwapiState } from '../../store/swapi.state';
+import { DataService } from '../../data.service';
 
 @Component({
   selector: 'app-planet-item',
@@ -6,10 +13,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planet-item.component.css']
 })
 export class PlanetItemComponent implements OnInit {
+  swapi$: Observable<SwapiState>;
+  id: number;
+  currentPlanet: any;
+  filmsAppeared: any[] = [];
+  residents: any[] = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private store: Store<SwapiState>,
+              private dataService: DataService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.swapi$ = this.store.select('swapi');
+
+    this.swapi$.subscribe((data) => {
+      if (this.verifyDataToLoad()) {
+        this.setCurrentPlanet();
+      }
+    });
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.setCurrentPlanet();
+        }
+      )
+  }
+
+  verifyDataToLoad() {
+    return !this.currentPlanet || 
+            this.filmsAppeared.length <= 0 ||
+            this.residents.length <= 0
+  }
+
+  setCurrentPlanet() {
+    this.currentPlanet = this.dataService.getPlanetsById(this.id);
+    if (this.currentPlanet) {
+      console.log(this.currentPlanet);
+      this.filmsAppeared = [];
+      this.dataService.getFilmsFromUrls(this.currentPlanet.films, this.filmsAppeared);
+      this.residents = [];
+      this.dataService.getCharactersFromUrls(this.currentPlanet.residents, this.residents);
+    }
   }
 
 }
