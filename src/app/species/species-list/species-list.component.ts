e2db8c41eaa6d17795d3bde45afcea53925e89f3
Vinicius Cechin <vinicechin@gmail.com>
2018-06-23 13:09:33 -0300

    Added species list component styles

--- src/app/species/species-list/species-list.component.ts
+++ src/app/species/species-list/species-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router'; 
+
+import { SwapiState } from '../../store/swapi.state';
 
 @Component({
   selector: 'app-species-list',
@@ -6,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./species-list.component.css']
 })
 export class SpeciesListComponent implements OnInit {
+  swapi$: Observable<any>;
+  species: any[];
+  searchTerm = '';
 
-  constructor() { }
+  constructor(private store: Store<SwapiState>,
+              private router: Router) { }
 
   ngOnInit() {
+    this.swapi$ = this.store.select('swapi');
+    this.swapi$.subscribe((data) => {
+      this.species = data.species.items;
+    });
   }
 
 }
