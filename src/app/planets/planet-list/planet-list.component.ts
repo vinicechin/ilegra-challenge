3e5dfc2d2ebf95417aaf9f28d3e698d0fe13af37
Vinicius Cechin <vinicechin@gmail.com>
2018-06-23 15:35:21 -0300

    Add planet list component styles

--- src/app/planets/planet-list/planet-list.component.ts
+++ src/app/planets/planet-list/planet-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router'; 
+
+import { SwapiState } from '../../store/swapi.state';
 
 @Component({
   selector: 'app-planet-list',
@@ -6,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planet-list.component.css']
 })
 export class PlanetListComponent implements OnInit {
+  swapi$: Observable<any>;
+  planets: any[];
+  searchTerm = '';
 
-  constructor() { }
+  constructor(private store: Store<SwapiState>,
+              private router: Router) { }
 
   ngOnInit() {
+    this.swapi$ = this.store.select('swapi');
+    this.swapi$.subscribe((data) => {
+      this.planets = data.planets.items;
+    });
   }
 
 }
