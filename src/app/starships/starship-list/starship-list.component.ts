b128d41ff058d361486ddd9bfc52efec0edc083e
Vinicius Cechin <vinicechin@gmail.com>
2018-06-23 18:17:23 -0300

    Add starship list component styles

--- src/app/starships/starship-list/starship-list.component.ts
+++ src/app/starships/starship-list/starship-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router'; 
+
+import { SwapiState } from '../../store/swapi.state';
 
 @Component({
   selector: 'app-starship-list',
@@ -6,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./starship-list.component.css']
 })
 export class StarshipListComponent implements OnInit {
+  swapi$: Observable<any>;
+  starships: any[];
+  searchTerm = '';
 
-  constructor() { }
+  constructor(private store: Store<SwapiState>,
+              private router: Router) { }
 
   ngOnInit() {
+    this.swapi$ = this.store.select('swapi');
+    this.swapi$.subscribe((data) => {
+      this.starships = data.starships.items;
+    });
   }
 
 }
