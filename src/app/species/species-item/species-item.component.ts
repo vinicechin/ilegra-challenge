4f82bea09f8c78145ea825e7afb24707e566a2b0
Vinicius Cechin <vinicechin@gmail.com>
2018-06-23 14:00:55 -0300

    Added species item style

--- src/app/species/species-item/species-item.component.ts
+++ src/app/species/species-item/species-item.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router'; 
+
+import { SwapiState } from '../../store/swapi.state';
+import { DataService } from '../../data.service';
 
 @Component({
   selector: 'app-species-item',
@@ -6,10 +13,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./species-item.component.css']
 })
 export class SpeciesItemComponent implements OnInit {
+  swapi$: Observable<SwapiState>;
+  id: number;
+  currentSpecies: any;
+  filmsAppeared: any[] = [];
+  members: any[] = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private store: Store<SwapiState>,
+              private dataService: DataService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.swapi$ = this.store.select('swapi');
+
+    this.swapi$.subscribe((data) => {
+      if (this.verifyDataToLoad()) {
+        this.setCurrentSpecies();
+      }
+    });
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.setCurrentSpecies();
+        }
+      )
   }
 
+  verifyDataToLoad() {
+    return !this.currentSpecies || 
+            this.filmsAppeared.length <= 0 ||
+            this.members.length <= 0
+  }
+
+  setCurrentSpecies() {
+    this.currentSpecies = this.dataService.getSpeciesById(this.id);
+    if (this.currentSpecies) {
+      console.log(this.currentSpecies);
+      this.filmsAppeared = [];
+      this.dataService.getFilmsFromUrls(this.currentSpecies.films, this.filmsAppeared);
+      this.members = [];
+      this.dataService.getCharactersFromUrls(this.currentSpecies.people, this.members);
+    }
+  }
 }
