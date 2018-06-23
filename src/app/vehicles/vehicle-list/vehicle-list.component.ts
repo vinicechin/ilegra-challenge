efb1e85a533ca590c216cc7c7618fed9ea0cb831
Vinicius Cechin <vinicechin@gmail.com>
2018-06-23 17:25:00 -0300

    Add vehicle list component styles

--- src/app/vehicles/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicles/vehicle-list/vehicle-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router'; 
+
+import { SwapiState } from '../../store/swapi.state';
 
 @Component({
   selector: 'app-vehicle-list',
@@ -6,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-list.component.css']
 })
 export class VehicleListComponent implements OnInit {
+  swapi$: Observable<any>;
+  vehicles: any[];
+  searchTerm = '';
 
-  constructor() { }
+  constructor(private store: Store<SwapiState>,
+              private router: Router) { }
 
   ngOnInit() {
+    this.swapi$ = this.store.select('swapi');
+    this.swapi$.subscribe((data) => {
+      this.vehicles = data.vehicles.items;
+    });
   }
 
 }
